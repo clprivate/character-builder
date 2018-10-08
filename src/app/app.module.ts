bbce800311226bd82f3640e42f7a418989f58b92
Looney <clooney@amazon.com>
2018-10-08 09:33:48 -0700

    Import AngularFireModule and AngularFireDatabaseModule into app.module.ts. imports: array not modified. Add to imports: at later time if needed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
