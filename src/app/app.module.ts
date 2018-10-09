d1410caaf2f3ec4fd6d05050e993ccd0012e2b65
Looney <clooney@amazon.com>
2018-10-09 08:36:24 -0700

    Update app.module (I don't remember how, run file diff if needed).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
-import { GameCharactersComponent } from './game-characters/game-characters.component'
+import { GameCharactersComponent } from './game-characters/game-characters.component';
+import { GameCharacterComponent } from './game-characters/game-character/game-character.component';
+import { GameCharacterListComponent } from './game-characters/game-character-list/game-character-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameCharactersComponent
+    GameCharactersComponent,
+    GameCharacterComponent,
+    GameCharacterListComponent
   ],
   imports: [
     BrowserModule,
