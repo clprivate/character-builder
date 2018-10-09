3aeba010507fe6229d3534683b8c26a90efa9b26
Looney <clooney@amazon.com>
2018-10-09 08:54:58 -0700

    Add properties to game-character.model.ts

--- src/app/game-characters/shared/game-character.model.ts
+++ src/app/game-characters/shared/game-character.model.ts
@@ -1,2 +1,21 @@
 export class GameCharacter {
+    $key: string;
+    name: string;
+    player: string;
+    gameClass: string;
+    
+    // 
+    hp: number;
+    armor: number;
+
+    // Attributes
+    mightScore: number;    
+    mightMod: number;
+    prowessScore: number;
+    prowessMod: number;
+    mindScore: number;
+    mindMod: number;
+    magicScore: number;
+    magicMod: number;
+
 }
