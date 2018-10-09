a4bc8608034efa8a9134a4dea3fc605ff01de4ee
Looney <clooney@amazon.com>
2018-10-09 09:28:07 -0700

    Implement getData() method in game-character-service.ts. The 'game-characters' argument passed into this.firebase.list may be wrong.

--- src/app/game-characters/shared/game-character.service.ts
+++ src/app/game-characters/shared/game-character.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { GameCharacter } from "./game-character.model";
 
 import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import { getViewData } from '@angular/core/src/render3/instructions';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,15 @@ import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 export class GameCharacterService {
   gameCharacterList: AngularFireList<any>;
   selectedGameCharacter: GameCharacter = new GameCharacter();
+
+  constructor(private firebase: AngularFireDatabase) {
+    
+
+  }
+
+  getData() {
+    this.gameCharacterList = this.firebase.list('game-characters'); // this game-characters argument might be wrong. Node name needed.
+    return this.gameCharacterList;
+  }
   
-  constructor() { }
 }
