a5ac038badaa44cef1fd8030a996a87ea5f6f64d
Looney <clooney@amazon.com>
2018-10-09 09:00:52 -0700

    Executed in game-character.service.ts: import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';

--- src/app/game-characters/shared/game-character.service.ts
+++ src/app/game-characters/shared/game-character.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { GameCharacter } from "./game-character.model";
 
+import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+
 @Injectable({
   providedIn: 'root'
 })
