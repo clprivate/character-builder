b8bb99af1409e7c95a534e9ae411bed383bff7b8
Looney <clooney@amazon.com>
2018-10-11 08:57:35 -0700

    Import GameCharacterService into game-characters.component.ts, add it to @component providers, inject gameCharacterService into constructor.

--- src/app/game-characters/game-characters.component.ts
+++ src/app/game-characters/game-characters.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { GameCharacterService } from './shared/game-character.service';
 
 @Component({
   selector: 'app-game-characters',
   templateUrl: './game-characters.component.html',
-  styleUrls: ['./game-characters.component.css']
+  styleUrls: ['./game-characters.component.css'],
+  providers: [GameCharacterService]
 })
+
 export class GameCharactersComponent implements OnInit {
 
   constructor() { }
